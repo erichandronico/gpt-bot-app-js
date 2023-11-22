054e1a62197e1d1d4ff182084d655036eadd1f0d
Erich Muñoz Ramos <erich.munoz@gmail.com>
2023-11-22 11:49:15 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,35 @@
+﻿## Documentación del Script de Chat
+
+### Función `initChat`
+- **Propósito**: Inicializar la funcionalidad del chat. Configura los elementos del DOM y los manejadores de eventos.
+- **Elementos del DOM**: `messageArea`, `messagesList`, `sendButton`, `refreshButton`, `deleteButton`.
+- **Eventos**: Agrega listeners a los botones para manejar el envío, la actualización y la eliminación de mensajes.
+
+### Función `handleSubmit`
+- **Propósito**: Manejar el envío de mensajes.
+- **Operación**: Recoge el mensaje del área de texto, lo envía al servidor usando una petición POST, actualiza la lista de mensajes con la respuesta, y limpia el área de texto.
+- **Manejo de Errores**: Captura y registra errores relacionados con el envío de mensajes.
+
+### Función `updateMessagesList`
+- **Propósito**: Actualizar la lista de mensajes en la interfaz de usuario.
+- **Operación**: Recibe un array de mensajes, los recorre y los añade al `messagesList` en el DOM.
+
+### Función `handleRefresh`
+- **Propósito**: Refrescar la lista de mensajes.
+- **Operación**: Realiza una petición GET para obtener los mensajes más recientes, los ordena y actualiza la lista de mensajes.
+- **Manejo de Errores**: Captura y registra errores relacionados con la actualización de mensajes.
+
+### Función `handleDelete`
+- **Propósito**: Borrar todos los mensajes.
+- **Operación**: Envía una petición DELETE al servidor para borrar los mensajes y luego limpia la lista de mensajes en la interfaz de usuario.
+- **Manejo de Errores**: Captura y registra errores relacionados con la eliminación de mensajes.
+
+### Función `clearMessagesList`
+- **Propósito**: Limpiar la lista de mensajes en la interfaz de usuario.
+- **Operación**: Elimina todo el contenido del `messagesList` en el DOM.
+
+### Event Listeners
+- **Operación**: Agrega listeners a `sendButton`, `refreshButton` y `deleteButton` para ejecutar `handleSubmit`, `handleRefresh` y `handleDelete`, respectivamente.
+
+### Ejecución
+- **Operación**: Ejecuta la función `initChat` cuando la página se haya cargado completamente.
---
+++ script.js
@@ -0,0 +1,71 @@
+﻿
+// Esta función inicializa el chat
+function initChat() {
+  // Referencias a elementos del DOM
+  const messageArea = document.getElementById('messageArea');
+  const messagesList = document.getElementById('messagesList');
+  const sendButton = document.getElementById('sendButton');
+  const refreshButton = document.getElementById('refreshButton');
+  const deleteButton = document.getElementById('deleteButton');
+
+  // Función para manejar el envío de mensajes
+  async function handleSubmit() {
+    const message = messageArea.value;
+    if (!message) return;
+
+    try {
+      const response = await axios.post('http://localhost:9000/chats', { chat: message });
+      const mensajes = _.sortBy(response?.data?.data, 'fecha');
+      updateMessagesList(mensajes);
+      messageArea.value = '';
+    } catch (error) {
+      console.error('Error al enviar el mensaje:', error);
+    }
+  }
+
+  // Función para actualizar la lista de mensajes
+  function updateMessagesList(messages) {
+    messagesList.innerHTML = '';
+    messages.forEach(msg => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${msg.fecha} [${msg.role}]: ${msg.content}`;
+      messagesList.appendChild(listItem);
+    });
+  }
+
+
+// Funciones para manejar la actualización y eliminación de mensajes
+async function handleRefresh() {
+    try {
+        const response = await axios.get('http://localhost:9000/chats');
+        const sortedMessages = _.sortBy(response?.data?.data, 'fecha');
+        updateMessagesList(sortedMessages);
+    } catch (error) {
+        console.error('Error al refrescar los mensajes:', error);
+    }
+}
+
+
+async function handleDelete() {
+    try {
+        await axios.delete('http://localhost:9000/chats');
+        clearMessagesList();
+    } catch (error) {
+        console.error('Error al borrar los mensajes:', error);
+    }
+}
+
+function clearMessagesList() {
+    const messagesList = document.getElementById('messagesList');
+    messagesList.innerHTML = '';                                    // Limpia el contenido del elemento
+}
+
+
+  // Agregar event listeners a los botones
+  sendButton.addEventListener('click', handleSubmit);
+  refreshButton.addEventListener('click', handleRefresh);
+  deleteButton.addEventListener('click', handleDelete);
+}
+
+// Ejecutar la función initChat cuando la página esté cargada
+document.addEventListener('DOMContentLoaded', initChat);
